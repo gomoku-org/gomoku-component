7f91ac4f2e896364b21a6a8286efdacb54ab6666
karez peshawa <karezpeshawa75@gmail.com>
2025-10-07 14:47:49 +0200

    feat: export components from src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-
-export { Regler } from '.';
-
-export {StartButton} from './components'
-
+export * from "./Board";
+export * from "./ResetButton";
+export * from "./StartButton";
+export * from "./Result";
+export * from "./Header";
+export * from "./InputField";
+export * from "./ChoosePiece";
+export * from "./ReglerButton";

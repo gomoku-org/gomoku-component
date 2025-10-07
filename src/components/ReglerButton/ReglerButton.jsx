de07d3edfc6648fadcfa61fac3a9a64f7a40e931
karez peshawa <karezpeshawa75@gmail.com>
2025-10-07 19:41:03 +0200

    fixa

--- src/components/ReglerButton/ReglerButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ReglerButton({ label = "Regler", onClick, style }) {
-  return (
-    <button type="button" onClick={onClick} style={style}>
-      {label}
-    </button>
-  );
-}

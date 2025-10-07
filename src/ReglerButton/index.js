7f9153f2d62979bf540157509503b0e1728ddb84
karez peshawa <karezpeshawa75@gmail.com>
2025-10-07 11:59:47 +0200

    chore: make package importable (move to src, add exports)

---
+++ src/ReglerButton/index.js
@@ -0,0 +1,1 @@
+export { default as ReglerButton } from "./ReglerButton.jsx";
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * from "./Board";
+export * from "./ResetButton";
+export * from "./StartButton";
+export * from "./ChoosePiece";
+export * from "./Header";
+export * from "./InputField";
+export * from "./ReglerButton";

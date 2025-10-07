a8aaa00f3abfdd692f2901de1e1033c43f19fca7
karez peshawa <karezpeshawa75@gmail.com>
2025-10-07 19:18:23 +0200

    fix: resolve conflict markers in src/index.js; export public API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-export * from "./Board";
-export * from "./ResetButton";
-export * from "./StartButton";
-<<<<<<< HEAD
-export * from "./Result";
-export * from "./Header";
-export * from "./InputField";
-export * from "./ChoosePiece";
-=======
-export * from "./ChoosePiece";
-export * from "./Header";
-export * from "./InputField";
->>>>>>> origin/main
-export * from "./ReglerButton";
+export { default as Board } from "./components/Board/Board";
+export { default as ResetButton } from "./components/ResetButton/ResetButton";
+export { default as StartButton } from "./components/StartButton/StartButton";
+export { default as Result } from "./components/Result/Result";
+export { default as Header } from "./components/Header/Header";
+export { default as InputField } from "./components/InputField/InputField";
+export { default as ChoosePiece } from "./components/ChoosePiece/ChoosePiece";
+export { default as ReglerButton } from "./components/ReglerButton/ReglerButton";

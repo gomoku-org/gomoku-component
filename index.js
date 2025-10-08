24d6421c19e88f7995406fcaaa617bb9a5197b8d
karez peshawa <karezpeshawa75@gmail.com>
2025-10-08 09:19:50 +0200

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-export { default as Board }        from "./components/Board/Board.jsx";
-export { default as ResetButton }  from "./components/ResetButton/ResetButton.jsx";
-export { default as StartButton }  from "./components/StartButton/StartButton.jsx";
-export { default as Result }       from "./components/Result/Result.jsx";
-export { default as Header }       from "./components/Header/Header.jsx";
-export { default as InputField }   from "./components/InputField/InputField.jsx";
-export { default as ChoosePiece }  from "./components/ChoosePiece/ChoosePiece.jsx";
-export { default as ReglerButton } from "./components/ReglerButton/ReglerButton.jsx";
+export { default as Board } from "./src/components/Board/Board.jsx";
+export { default as StartButton } from "./src/components/StartButton/StartButton.jsx";
+export { default as ResetButton } from "./src/components/ResetButton/ResetButton.jsx";
+export { default as Result } from "./src/components/Result/Result.jsx";
+export { default as ScoreBoard } from "./src/components/ScoreBoard/ScoreBoard.jsx";
+export { default as ReglerButton } from "./ReglerButton";
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,10 @@
 
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.css";
 
-const Header = () => {
+const Header = ({ title = "Welcome to Gomoku! "}) => {
 
- return(
-
-	<>Welcome to Gomoku!</>
-
- );
-
+ return <h1 className={styles.headerTitle}>{title}</h1>;
 };
 
 export default Header;
